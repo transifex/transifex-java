717a9b3b59b6e9d5a643cc8236d1e6d44dab8b14
Petros Douvantzis <petrakeas@gmail.com>
2021-02-16 14:04:52 +0200

    Added pull command in cli
    CDSHandler has a low-level fetch method that
    allows the caller to acces the translation input stream
    from the server. The existing fetch method uses it under the hood.
    
    TranslationsDownloader makes use of the new fetch method to
    save the translations as files in the specified directory.
    
    Added pull command in cli.
    
    Moved token and secret option inside the subcommands.
    
    Updated the cli section of the readme.
    
    Added unit tests.

--- TransifexNativeSDK/clitool/src/test/java/com/transifex/clitool/MainClassTest.java
+++ TransifexNativeSDK/clitool/src/test/java/com/transifex/clitool/MainClassTest.java
@@ -2,12 +2,16 @@ package com.transifex.clitool;
 
 import com.google.gson.Gson;
 import com.transifex.common.LocaleData;
+import com.transifex.common.Utils;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import androidx.annotation.NonNull;
 import okhttp3.mockwebserver.Dispatcher;
@@ -21,11 +25,16 @@ public class MainClassTest {
 
     private MockWebServer server = null;
     private String baseUrl = null;
+    File tempDir = new File("build" + File.separator + "unitTestTempDir");
 
     @Before
     public void setUp() {
         server = new MockWebServer();
         baseUrl = server.url("").toString();
+
+        if (tempDir.exists()) {
+            Utils.deleteDirectory(tempDir);
+        }
     }
 
     @After
@@ -35,9 +44,57 @@ public class MainClassTest {
                 server.shutdown();
             } catch (IOException ignored) {}
         }
+
+        if (tempDir.exists()) {
+            boolean deleted = Utils.deleteDirectory(tempDir);
+            if (!deleted) {
+                System.out.println("Could not delete tmp dir after test. Next test may fail.");
+            }
+        }
     }
 
-    private Dispatcher getPostDispatcher() {
+    public static final String elBody = "{\"data\":{\"test_key\":{\"string\":\"Καλημέρα\"},\"another_key\":{\"string\":\"Καλό απόγευμα\"},\"key3\":{\"string\":\"\"}}}";
+    public static final String esBody = "{\"data\":{\"test_key\":{\"string\":\"Buenos días\"},\"another_key\":{\"string\":\"Buenas tardes\"},\"key3\":{\"string\":\"\"}}}";
+
+    public static Dispatcher getElEsDispatcher() {
+        Dispatcher dispatcher = new Dispatcher() {
+
+            @NonNull
+            @Override
+            public MockResponse dispatch (RecordedRequest request) throws InterruptedException {
+
+                switch (request.getPath()) {
+                    case "/content/el":
+                        return new MockResponse().setResponseCode(200).setBody(elBody);
+                    case "/content/es":
+                        return new MockResponse().setResponseCode(200).setBody(esBody);
+                }
+                return new MockResponse().setResponseCode(404);
+            }
+        };
+
+        return dispatcher;
+    }
+
+    public static Dispatcher getElDispatcher() {
+        Dispatcher dispatcher = new Dispatcher() {
+
+            @NonNull
+            @Override
+            public MockResponse dispatch (RecordedRequest request) throws InterruptedException {
+
+                switch (request.getPath()) {
+                    case "/content/el":
+                        return new MockResponse().setResponseCode(200).setBody(elBody);
+                }
+                return new MockResponse().setResponseCode(404);
+            }
+        };
+
+        return dispatcher;
+    }
+
+    public static Dispatcher getPostDispatcher() {
         Dispatcher dispatcher = new Dispatcher() {
 
             @NonNull
@@ -67,7 +124,7 @@ public class MainClassTest {
 
     @Test
     public void testPush_noSecret() {
-        String args = "-t token push -m app";
+        String args = "push -t token -m app";
         int returnValue = MainClass.testMain(args);
 
         assertThat(returnValue).isEqualTo(2);
@@ -75,12 +132,11 @@ public class MainClassTest {
 
     @Test
     public void testPush_wrongModuleName() {
-        String args = "-t token -s secret push -m app2";
+        String args = "push -t token -s secret -m app2";
         int returnValue = MainClass.testMain(args);
 
         assertThat(returnValue).isEqualTo(1);
     }
-
 
     @Test
     public void testPush_useFile() {
@@ -89,7 +145,7 @@ public class MainClassTest {
 
         server.setDispatcher(getPostDispatcher());
 
-        String args = String.format("-t token -s secret -u %s push -f %s", baseUrl,
+        String args = String.format("-u %s push -t token -s secret -f %s", baseUrl,
                 "../txsdk/src/test/res/values/strings.xml");
         int returnValue = MainClass.testMain(args);
 
@@ -117,7 +173,7 @@ public class MainClassTest {
 
         server.setDispatcher(getPostDispatcher());
 
-        String args = String.format("-t token -s secret -u %s push -f %s -f %s", baseUrl,
+        String args = String.format("-u %s push -t token -s secret -f %s -f %s", baseUrl,
                 "../txsdk/src/test/res/values/strings.xml",
                 "../txsdk/src/test/res/values-el/strings.xml");
         int returnValue = MainClass.testMain(args);
@@ -142,7 +198,7 @@ public class MainClassTest {
     public void testClear() {
         server.setDispatcher(getPostDispatcher());
 
-        String args = String.format("-t token -s secret -u %s clear", baseUrl);
+        String args = String.format("-u %s clear -t token -s secret", baseUrl);
         int returnValue = MainClass.testMain(args);
 
         assertThat(returnValue).isEqualTo(0);
@@ -159,5 +215,83 @@ public class MainClassTest {
         LocaleData.TxPostData parsedPostData = gson.fromJson(postBody, LocaleData.TxPostData.class);
         assertThat(parsedPostData.meta.purge).isTrue();
         assertThat(parsedPostData.data).isEmpty();
+    }
+
+    @Test
+    public void testPull_normal() {
+        server.setDispatcher(getElEsDispatcher());
+
+        String args = String.format("-u %s pull -t token -l el es -d %s", baseUrl, tempDir.getPath());
+        int returnValue = MainClass.testMain(args);
+
+        assertThat(returnValue).isEqualTo(0);
+
+        File elFile = Paths.get(tempDir.getPath(), MainClass.OUT_DIR_NAME, "el", MainClass.OUT_FILE_NAME).toFile();
+        String elString = null;
+        try {
+            elString = Utils.readInputStream(new FileInputStream(elFile));
+        } catch (IOException ignored) {}
+        assertThat(elString).isNotNull();
+        assertThat(elString).isEqualTo(elBody);
+
+        File esFile = Paths.get(tempDir.getPath(), MainClass.OUT_DIR_NAME, "es", MainClass.OUT_FILE_NAME).toFile();
+        String esString = null;
+        try {
+            esString = Utils.readInputStream(new FileInputStream(esFile));
+        } catch (IOException ignored) {}
+        assertThat(esString).isNotNull();
+        assertThat(esString).isEqualTo(esBody);
+    }
+
+    @Test
+    public void testPull_consecutiveRuns() {
+        server.setDispatcher(getElEsDispatcher());
+
+        String args = String.format("-u %s pull -t token -l el -d %s", baseUrl, tempDir.getPath());
+        int returnValue = MainClass.testMain(args);
+
+        assertThat(returnValue).isEqualTo(0);
+
+        args = String.format("-u %s pull -t token -l es -d %s", baseUrl, tempDir.getPath());
+        returnValue = MainClass.testMain(args);
+
+        assertThat(returnValue).isEqualTo(0);
+
+        File elFile = Paths.get(tempDir.getPath(), MainClass.OUT_DIR_NAME, "el", MainClass.OUT_FILE_NAME).toFile();
+        String elString = null;
+        try {
+            elString = Utils.readInputStream(new FileInputStream(elFile));
+        } catch (IOException ignored) {}
+        assertThat(elString).isNotNull();
+        assertThat(elString).isEqualTo(elBody);
+
+        File esFile = Paths.get(tempDir.getPath(), MainClass.OUT_DIR_NAME, "es", MainClass.OUT_FILE_NAME).toFile();
+        String esString = null;
+        try {
+            esString = Utils.readInputStream(new FileInputStream(esFile));
+        } catch (IOException ignored) {}
+        assertThat(esString).isNotNull();
+        assertThat(esString).isEqualTo(esBody);
+    }
+
+    @Test
+    public void testPull_onlyElInResponse() {
+        server.setDispatcher(getElDispatcher());
+
+        String args = String.format("-u %s pull -t token -l el es -d %s", baseUrl, tempDir.getPath());
+        int returnValue = MainClass.testMain(args);
+
+        assertThat(returnValue).isEqualTo(1);
+
+        File elFile = Paths.get(tempDir.getPath(), MainClass.OUT_DIR_NAME, "el", MainClass.OUT_FILE_NAME).toFile();
+        String elString = null;
+        try {
+            elString = Utils.readInputStream(new FileInputStream(elFile));
+        } catch (IOException ignored) {}
+        assertThat(elString).isNotNull();
+        assertThat(elString).isEqualTo(elBody);
+
+        File esFile = Paths.get(tempDir.getPath(), MainClass.OUT_DIR_NAME, "es", MainClass.OUT_FILE_NAME).toFile();
+        assertThat(esFile.exists()).isFalse();
     }
 }
---
+++ TransifexNativeSDK/common/src/test/java/com/transifex/common/TranslationsDownloaderTest.java
@@ -0,0 +1,220 @@
+package com.transifex.common;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class TranslationsDownloaderTest {
+
+    private MockWebServer server = null;
+    private String baseUrl = null;
+    File tempDir = new File("build" + File.separator + "unitTestTempDir");
+
+    @Before
+    public void setUp() {
+        server = new MockWebServer();
+        baseUrl = server.url("").toString();
+
+        if (tempDir.exists()) {
+            Utils.deleteDirectory(tempDir);
+        }
+    }
+
+    @After
+    public void Teardown() {
+        if (server != null) {
+            try {
+                server.shutdown();
+            } catch (IOException ignored) {}
+        }
+
+        if (tempDir.exists()) {
+            boolean deleted = Utils.deleteDirectory(tempDir);
+            if (!deleted) {
+                System.out.println("Could not delete tmp dir after test. Next test may fail.");
+            }
+        }
+    }
+
+    @Test
+    public void testSaveTranslations_dirDoesNotExist() {
+        String[] localeCodes = new String[]{"el", "es"};
+        CDSHandler cdsHandler = new CDSHandler(localeCodes, "token", null, baseUrl);
+        TranslationsDownloader downloader = new TranslationsDownloader(cdsHandler);
+
+        HashMap<String, File> translationFiles = downloader.downloadTranslations(null, tempDir, "strings.txt");
+
+        assertThat(translationFiles).isNotNull();
+        assertThat(translationFiles).isEmpty();
+    }
+
+    @Test
+    public void testSaveTranslations_normalResponse() {
+        server.setDispatcher(CDSHandlerTest.getElEsDispatcher());
+
+        boolean tempDirCreated =  tempDir.mkdirs();
+        assertThat(tempDirCreated).isTrue();
+
+        String[] localeCodes = new String[]{"el", "es"};
+        CDSHandler cdsHandler = new CDSHandler(localeCodes, "token", null, baseUrl);
+        TranslationsDownloader downloader = new TranslationsDownloader(cdsHandler);
+
+        HashMap<String, File> translationFiles = downloader.downloadTranslations(null, tempDir, "strings.txt");
+
+        RecordedRequest recordedRequest = null;
+        try {
+            recordedRequest = server.takeRequest();
+        } catch (InterruptedException ignored) {
+        }
+        assertThat(recordedRequest).isNotNull();
+        assertThat(recordedRequest.getMethod()).isEqualTo("GET");
+        assertThat(recordedRequest.getHeader("Authorization")).isEqualTo("Bearer token");
+        assertThat(recordedRequest.getHeader("Content-Type")).isEqualTo("application/json; charset=utf-8");
+
+        assertThat(translationFiles).isNotNull();
+        assertThat(translationFiles.keySet()).containsExactly("el", "es");
+        assertThat(translationFiles.get("el").getName()).isEqualTo("strings.txt");
+        assertThat(translationFiles.get("es").getName()).isEqualTo("strings.txt");
+
+        String elString = null;
+        try {
+            elString = Utils.readInputStream(new FileInputStream(translationFiles.get("el")));
+        } catch (IOException ignored) {}
+
+        assertThat(elString).isNotNull();
+        assertThat(elString).isEqualTo(CDSHandlerTest.elBody);
+
+        String esString = null;
+        try {
+            esString = Utils.readInputStream(new FileInputStream(translationFiles.get("es")));
+        } catch (IOException ignored) {}
+
+        assertThat(esString).isNotNull();
+        assertThat(esString).isEqualTo(CDSHandlerTest.esBody);
+    }
+
+    @Test
+    public void testSaveTranslations_onlyElInResponse() {
+        server.setDispatcher(CDSHandlerTest.getElDispatcher());
+
+        boolean tempDirCreated =  tempDir.mkdirs();
+        assertThat(tempDirCreated).isTrue();
+
+        String[] localeCodes = new String[]{"el", "es"};
+        CDSHandler cdsHandler = new CDSHandler(localeCodes, "token", null, baseUrl);
+        TranslationsDownloader downloader = new TranslationsDownloader(cdsHandler);
+
+        HashMap<String, File> translationFiles = downloader.downloadTranslations(null, tempDir, "strings.txt");
+        assertThat(translationFiles).isNotNull();
+        assertThat(translationFiles.keySet()).containsExactly("el");
+
+        String elString = null;
+        try {
+            elString = Utils.readInputStream(new FileInputStream(translationFiles.get("el")));
+        } catch (IOException ignored) {}
+
+        assertThat(elString).isNotNull();
+        assertThat(elString).isEqualTo(CDSHandlerTest.elBody);
+    }
+
+    @Test
+    public void testSaveTranslations_specifyLocale_normalResponse() {
+        server.setDispatcher(CDSHandlerTest.getElEsDispatcher());
+
+        boolean tempDirCreated =  tempDir.mkdirs();
+        assertThat(tempDirCreated).isTrue();
+
+        String[] localeCodes = new String[]{"el", "es"};
+        CDSHandler cdsHandler = new CDSHandler(localeCodes, "token", null, baseUrl);
+        TranslationsDownloader downloader = new TranslationsDownloader(cdsHandler);
+
+        HashMap<String, File> translationFiles = downloader.downloadTranslations("el", tempDir, "strings.txt");
+        assertThat(translationFiles).isNotNull();
+        assertThat(translationFiles.keySet()).containsExactly("el");
+
+        String elString = null;
+        try {
+            elString = Utils.readInputStream(new FileInputStream(translationFiles.get("el")));
+        } catch (IOException ignored) {}
+
+        assertThat(elString).isNotNull();
+        assertThat(elString).isEqualTo(CDSHandlerTest.elBody);
+    }
+
+    @Test
+    public void testSaveTranslations_overwriteExistingFile() {
+        server.setDispatcher(CDSHandlerTest.getElDispatcher());
+
+        File localeDir = new File(tempDir.getAbsoluteFile() + File.separator + "el");
+        boolean localeDirCreated =  localeDir.mkdirs();
+        assertThat(localeDirCreated).isTrue();
+
+        File dummyElStringFile = new File(localeDir + File.separator + "strings.txt");
+        try {
+            FileOutputStream outputStream = new FileOutputStream(dummyElStringFile);
+            String dummyContent = "some text";
+            outputStream.write(dummyContent.getBytes(StandardCharsets.UTF_8));
+            outputStream.close();
+        } catch (IOException ignored) {}
+
+        String[] localeCodes = new String[]{"el"};
+        CDSHandler cdsHandler = new CDSHandler(localeCodes, "token", null, baseUrl);
+        TranslationsDownloader downloader = new TranslationsDownloader(cdsHandler);
+
+        HashMap<String, File> translationFiles = downloader.downloadTranslations(null, tempDir, "strings.txt");
+        assertThat(translationFiles).isNotNull();
+        assertThat(translationFiles.keySet()).containsExactly("el");
+
+        String elString = null;
+        try {
+            elString = Utils.readInputStream(new FileInputStream(translationFiles.get("el")));
+        } catch (IOException ignored) {}
+
+        assertThat(elString).isNotNull();
+        assertThat(elString).isEqualTo(CDSHandlerTest.elBody);
+    }
+
+    @Test
+    public void testSaveTranslations_skipExistingFileIfError() {
+        server.setDispatcher(CDSHandlerTest.getElDispatcher());
+
+        File localeDir = new File(tempDir.getAbsoluteFile() + File.separator + "es");
+        boolean localeDirCreated =  localeDir.mkdirs();
+        assertThat(localeDirCreated).isTrue();
+
+        File dummyEsStringFile = new File(localeDir + File.separator + "strings.txt");
+        String dummyContent = "some text";
+        try {
+            FileOutputStream outputStream = new FileOutputStream(dummyEsStringFile);
+            outputStream.write(dummyContent.getBytes(StandardCharsets.UTF_8));
+            outputStream.close();
+        } catch (IOException ignored) {}
+
+        String[] localeCodes = new String[]{"es"};
+        CDSHandler cdsHandler = new CDSHandler(localeCodes, "token", null, baseUrl);
+        TranslationsDownloader downloader = new TranslationsDownloader(cdsHandler);
+
+        HashMap<String, File> translationFiles = downloader.downloadTranslations(null, tempDir, "strings.txt");
+        assertThat(translationFiles).isEmpty();
+
+        String esString = null;
+        try {
+            esString = Utils.readInputStream(new FileInputStream(dummyEsStringFile));
+        } catch (IOException ignored) {}
+
+        assertThat(esString).isNotNull();
+        assertThat(esString).isEqualTo(dummyContent);
+    }
+}
